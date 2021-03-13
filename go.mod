67a0b8871edc5b8f8863602e554bc3e427bf1e45
hammerandnail <yelionel26@gmail.com>
2021-03-13 21:34:26 +0800

    27. Enabling Go Modules and refactoring our code to use packages

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hardliver/go-web
+
+go 1.16
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,17 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"github.com/hardliver/go-web/pkg/render"
 )
 
 // Home is the home handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
